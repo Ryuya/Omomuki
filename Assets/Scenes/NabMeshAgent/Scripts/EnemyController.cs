e258c187271640084f730adc1980e8102bd01177
Ryuya <main.higashi.pg@gmail.com>
2020-04-30 00:23:49 +0900

    NavMesh Agent

---
+++ Assets/Scenes/NabMeshAgent/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player").GetComponent<PlayerController>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _agent.SetDestination(player.position);
+    }
+}
---
+++ Assets/Scenes/NabMeshAgent/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent agent;
+    
+    private GameManager _gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameManager = GameObject.Find("Scripts").GetComponent<GameManager>();
+        
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(_gameManager.target);
+    }
+}
